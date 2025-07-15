ccf4b9bdf7642259988094fb0e0551b64f1fac5b
Caleb WANG <caleb.wang@airudder.com>
2025-07-15 14:48:21 +0800

    feat: 增加一版简单的node selection的奖励设计

--- verl/utils/reward_score/__init__.py
+++ verl/utils/reward_score/__init__.py
@@ -83,6 +83,11 @@ def default_compute_score(data_source, solution_str, ground_truth, extra_info=No
         from . import search_r1_like_qa_em
 
         res = search_r1_like_qa_em.compute_score(solution_str, ground_truth)
+
+    elif data_source in ["Node-Selection-V5_DAPO-v1"]:
+        from . import node_selection
+        res = node_selection.compute_score(solution_str, ground_truth)
+
     else:
         raise NotImplementedError(f"Reward function is not implemented for {data_source=}")
 
